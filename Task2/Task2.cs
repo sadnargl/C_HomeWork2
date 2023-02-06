e574e419f575f56394818d56516687afe14b5637
Aleksey.Alekseev <sadnargl@gmail.com>
2023-02-06 12:38:53 +0300

    задача 13

---
+++ Task2/Task2.cs
@@ -0,0 +1,17 @@
+﻿Console.Write ("Введите число: ");
+if (int.TryParse (Console.ReadLine(), out int i))
+{
+    if (i<0) i= Math.Abs(i);
+    if (i.ToString().Length <= 2)
+    {
+        Console.WriteLine ("Третьей цифры нет");  
+    }
+    else
+    {
+        Console.WriteLine ($"Третья цифра числа: {i.ToString()[2]}"); 
+    }
+}
+else
+{
+Console.WriteLine ("Вы ввели не числo, либо не целое число, либо слишком большое число");        
+}
