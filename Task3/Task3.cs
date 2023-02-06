f2f59eed07ec7526741909d36e24cc753b4e055b
Aleksey.Alekseev <sadnargl@gmail.com>
2023-02-06 12:39:09 +0300

    задача 15

---
+++ Task3/Task3.cs
@@ -0,0 +1,26 @@
+﻿//С обработкой исключений
+
+Console.Write ("Введите число, обозначающее день недели: ");
+if (int.TryParse (Console.ReadLine(), out int i))
+{
+    if ((i.ToString().Length == 1) && (i<=7) && (i!=0))
+    {
+        if ((i == 6) || (i == 7))
+        {
+            Console.WriteLine ("Это выходной"); 
+        }
+        else
+        {
+            Console.WriteLine ("Это будний день"); 
+        } 
+    }
+    else
+    {
+        Console.WriteLine ("Вы ввели число вне диапазона 1-7");
+    }
+}
+else
+{
+Console.WriteLine ("Вы ввели не числo, либо не целое число, либо слишком большое число");     
+}
+
